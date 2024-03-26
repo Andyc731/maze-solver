2d4b1d24976f7abcb04bfa08b6f0d81fb467221b
Andy <andychc731@gmail.com>
2024-03-26 11:16:31 -0600

    draw cell on window

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from graphics import Window
-from line import Line
-from point import Point
+from cell import Cell
+from graphics import Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
-    point1 = Point(20, 40)
-    point2 = Point(50, 20)
-    line = Line(point1, point2)
-    win.draw_line(line, "blue")
+    cell = Cell(20, 20, 40, 40, win)
+    cell.draw()
     win.wait_for_close()
 
 
