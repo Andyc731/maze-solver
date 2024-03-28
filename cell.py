1b9db94d6f67167ba689308ac861b325e9eecd83
Andy <andychc731@gmail.com>
2024-03-28 12:05:18 -0600

    implement draw_move, draw white line if no wall

--- cell.py
+++ cell.py
@@ -2,7 +2,7 @@ from graphics import Line, Point, Window
 
 
 class Cell:
-    def __init__(self, x1, y1, x2, y2, win):
+    def __init__(self, x1, y1, x2, y2, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -12,8 +12,11 @@ class Cell:
         self._x2 = x2
         self._y2 = y2
         self._win = win
+        self.visited = False
 
     def draw(self):
+        if self._win is None:
+            return
         if self.has_left_wall:
             point1 = Point(self._x1, self._y1)
             point2 = Point(self._x1, self._y2)
@@ -37,3 +40,37 @@ class Cell:
             point2 = Point(self._x2, self._y2)
             line = Line(point1, point2)
             self._win.draw_line(line, "blue")
+        if not self.has_left_wall:
+            point1 = Point(self._x1, self._y1)
+            point2 = Point(self._x1, self._y2)
+            line = Line(point1, point2)
+            self._win.draw_line(line, "white")
+
+        if not self.has_top_wall:
+            point1 = Point(self._x1, self._y1)
+            point2 = Point(self._x2, self._y1)
+            line = Line(point1, point2)
+            self._win.draw_line(line, "white")
+
+        if not self.has_right_wall:
+            point1 = Point(self._x2, self._y1)
+            point2 = Point(self._x2, self._y2)
+            line = Line(point1, point2)
+            self._win.draw_line(line, "white")
+
+        if not self.has_bottom_wall:
+            point1 = Point(self._x1, self._y2)
+            point2 = Point(self._x2, self._y2)
+            line = Line(point1, point2)
+            self._win.draw_line(line, "white")
+
+    def draw_move(self, to_cell, undo=False):
+        if self._win is None:
+            return
+        point1 = Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2)
+        point2 = Point((to_cell._x1 + to_cell._x2) / 2, (to_cell._y1 + to_cell._y2) / 2)
+        line = Line(point1, point2)
+        color = "red"
+        if undo:
+            color = "gray"
+        self._win.draw_line(line, color)
